b2ee073effe1201664e5130a17e95b5201d2ae8b
OneAndonlyFinbar <68970363+OneAndonlyFinbar@users.noreply.github.com>
2021-08-12 22:18:32 -0400

    Update dataApi.js

--- dataApi.js
+++ dataApi.js
@@ -47,7 +47,7 @@ app.get('/api', function (req, res) {
 
 
     if (queryDataType === 'global') {
-        const raw = JSON.parse(fs.readFileSync('../data/globals.json', 'utf8'));
+        const raw = JSON.parse(fs.readFileSync('./data/globals.json', 'utf8'));
         const foundData = raw[data];
         if(!foundData) return res.send('{"success" : false, "error" :' + `"Couldnt find global data: ${data}"}`);
 
@@ -55,7 +55,7 @@ app.get('/api', function (req, res) {
     }
 
     if (queryDataType === 'server') {
-        const raw = JSON.parse(fs.readFileSync('../data/servers.json', 'utf8'));
+        const raw = JSON.parse(fs.readFileSync('./data/servers.json', 'utf8'));
         if(!raw[server]) return res.send('{"success" : false, "error" :' + `"Couldnt find server data: ${data} in server: ${server}"}`);
 
         const foundData = raw[server][data];
@@ -65,7 +65,7 @@ app.get('/api', function (req, res) {
     }
 
     if (queryDataType === 'member') {
-        const raw = JSON.parse(fs.readFileSync('../data/players.json', 'utf8'));
+        const raw = JSON.parse(fs.readFileSync('./data/players.json', 'utf8'));
         if (!raw[member]) return res.send('{"success" : false, "error" :' + `"Couldnt find member data: ${data} for member: ${member}"}`);
 
         const foundData = raw[member][data];
